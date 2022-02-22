e5f1f6ed4b3db646d37dcb402824dab513c7c8a9
Samuele De Tomasi <el3um4s@gmail.com>
2022-02-22 23:22:35 +0100

    Udpated readme

--- src/frontend/Components/Pages/Help.svelte
+++ src/frontend/Components/Pages/Help.svelte
@@ -30,39 +30,44 @@
     console.log(data);
     tables = [...data];
   });
+
+  let showInProduction = false;
 </script>
 
 <section transition:slide>
   <h1>Help</h1>
 
-  <input type="file" bind:files />
+  <p>TO DO...</p>
+  {#if showInProduction}
+    <input type="file" bind:files />
 
-  <button on:click={() => (showFileName = !showFileName)}>File Name</button>
-  <button on:click={() => (showCustomSQL = !showCustomSQL)}>SQL Query</button>
-  {#if tables.length > 0}
-    <button on:click={() => (showListTable = !showListTable)}
-      >List Tables</button
-    >{/if}
-  {#if files && files[0] && showFileName}
-    <p>
-      {files[0].name}
-    </p>
-  {/if}
+    <button on:click={() => (showFileName = !showFileName)}>File Name</button>
+    <button on:click={() => (showCustomSQL = !showCustomSQL)}>SQL Query</button>
+    {#if tables.length > 0}
+      <button on:click={() => (showListTable = !showListTable)}
+        >List Tables</button
+      >{/if}
+    {#if files && files[0] && showFileName}
+      <p>
+        {files[0].name}
+      </p>
+    {/if}
 
-  {#if showCustomSQL}
-    <button
-      on:click={() =>
-        globalThis.api.nodeAdodb.send("query", { source: source, sql })}
-      >Execute query</button
-    >
-    <textarea bind:value={sql} />
+    {#if showCustomSQL}
+      <button
+        on:click={() =>
+          globalThis.api.nodeAdodb.send("query", { source: source, sql })}
+        >Execute query</button
+      >
+      <textarea bind:value={sql} />
+    {/if}
+    {#if showListTable}
+      {#each tables as table}
+        <p>{table.TABLE_NAME}</p>
+      {/each}
+    {/if}
+    <div />
   {/if}
-  {#if showListTable}
-    {#each tables as table}
-      <p>{table.TABLE_NAME}</p>
-    {/each}
-  {/if}
-  <div />
 </section>
 
 <style lang="postcss">
--- src/frontend/Components/Pages/Info.svelte
+++ src/frontend/Components/Pages/Info.svelte
@@ -4,7 +4,7 @@
   import CheckForUpdate from "../Default/CheckForUpdate.svelte";
 
   const isElectron = globalThis?.api?.systemInfo ? true : false;
-  export let version = "0.0.10";
+  export let version = "0.0.11";
 </script>
 
 <section transition:slide>
