3ffe81871a39a46c1dd41740344b902255f2e83a
Samuele de Tomasi <el3um4s@gmail.com>
2022-02-23 13:55:41 +0100

    Update version number

--- src/frontend/Components/Pages/Info.svelte
+++ src/frontend/Components/Pages/Info.svelte
@@ -4,7 +4,7 @@
   import CheckForUpdate from "../Default/CheckForUpdate.svelte";
 
   const isElectron = globalThis?.api?.systemInfo ? true : false;
-  export let version = "0.0.12";
+  export let version = "0.0.13";
 </script>
 
 <section transition:slide>
