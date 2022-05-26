5488d33ad182645b4a8da0d1c98dddc1e85a5cfa
Samuele De Tomasi <el3um4s@gmail.com>
2022-05-26 12:24:29 +0200

    Refractored web page

--- src/frontend/Components/Pages/WebPage.svelte
+++ src/frontend/Components/Pages/WebPage.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import type { HistoryBrowser } from "../../Interfaces/StatusInterface";
+
   import { flip } from "svelte/animate";
   import { quintOut } from "svelte/easing";
 
@@ -13,6 +15,12 @@
 
   let src = $status.urlBrowser;
   let onlyStarred = false;
+  let textSearch = "";
+
+  $: historyBrowser = historyFiltered($status.historyBrowser, {
+    onlyStarred,
+    textSearch,
+  });
 
   const openWebPage = async (url: string) => {
     status.componentVisible(LoadingPage);
@@ -31,13 +39,39 @@
       starred: false,
     });
   };
+
+  function historyFiltered(
+    historyBrowser: HistoryBrowser[],
+    settings: { onlyStarred: boolean; textSearch: string }
+  ) {
+    const { onlyStarred } = { ...settings };
+    let history = historyBrowser;
+    if (onlyStarred) {
+      history = history.filter((el) => el.starred == true);
+    }
+
+    if (textSearch.trim() != "") {
+      history = history.filter(
+        (el) =>
+          el.note.includes(textSearch) ||
+          el.title.includes(textSearch) ||
+          el.url.includes(textSearch)
+      );
+    }
+    return history;
+  }
 </script>
 
 <section transition:slide>
   <h1>Open Web Page</h1>
 
   <div>
-    <input type="url" bind:value={src} placeholder="https://example.com" />
+    <input
+      class="inputUrl"
+      type="url"
+      bind:value={src}
+      placeholder="https://example.com"
+    />
     <button
       on:click={async () => {
         openWebPage(src);
@@ -46,7 +80,11 @@
       <Fa icon={faPlay} />
     </button>
   </div>
-  <div>
+  <div class="filterBar">
+    <div>Shown {historyBrowser.length} of {$status.historyBrowser.length}</div>
+    <div>
+      <input type="text" bind:value={textSearch} placeholder="Search text" />
+    </div>
     <div>
       <button
         on:click={() => {
@@ -56,14 +94,12 @@
         ><Fa icon={onlyStarred ? faStar : faStarLight} /></button
       >
     </div>
+  </div>
+  <div class="listHistory">
     <ul>
-      {#each $status.historyBrowser as item (item.url)}
+      {#each historyBrowser as item (item.url)}
         <li animate:flip={{ duration: 250, easing: quintOut }}>
-          {#if onlyStarred && item.starred}
-            <CardHistoryBrowser {item} />
-          {:else if !onlyStarred}
-            <CardHistoryBrowser {item} />
-          {/if}
+          <CardHistoryBrowser {item} />
         </li>
       {/each}
     </ul>
@@ -72,12 +108,39 @@
 
 <style lang="postcss">
   section {
-    @apply p-2;
+    @apply p-2 h-full flex flex-col;
   }
-  div {
-    @apply m-4;
-  }
-  input {
+
+  .inputUrl {
     width: calc(100% - 64px);
+  }
+
+  .filterBar {
+    @apply flex flex-row p-2 gap-2 items-center;
+  }
+
+  .listHistory {
+    @apply h-full;
+    overflow-y: overlay;
+    overflow-x: hidden;
+  }
+
+  ul {
+    width: calc(100% - 16px);
+  }
+
+  .listHistory::-webkit-scrollbar {
+    @apply w-4;
+  }
+
+  .listHistory::-webkit-scrollbar-track {
+    border: 2px solid var(--background-color);
+    border-left: 8px solid var(--background-color);
+    background-color: var(--background-color);
+  }
+
+  .listHistory::-webkit-scrollbar-thumb {
+    background-color: var(--hover-scrollbar-thumb);
+    border-left: 8px solid var(--background-color);
   }
 </style>
