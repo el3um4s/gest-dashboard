b8b1f3ae4a4a2d7d3a72bc458bc4ddb3b0a7e26a
Samuele De Tomasi <el3um4s@gmail.com>
2023-01-11 14:15:32 +0100

    Update Dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,15 +2,19 @@ import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
-import {
-	terser
-} from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
-import css from 'rollup-plugin-css-only';
+import css from "@el3um4s/rollup-plugin-css-only";
+import {
+	terser
+} from "@el3um4s/rollup-plugin-terser";
 import copy from 'rollup-plugin-copy';
 import postcss from 'rollup-plugin-postcss';
 import json from "@rollup/plugin-json";
+import {
+	spawn
+} from "child_process";
+
 
 
 const production = !process.env.ROLLUP_WATCH;
@@ -25,9 +29,9 @@ function serve() {
 	return {
 		writeBundle() {
 			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
+			server = spawn("npm", ["run", "start", "--", "--dev"], {
+				stdio: ["ignore", "inherit", "inherit"],
+				shell: true,
 			});
 
 			process.on('SIGTERM', toExit);
